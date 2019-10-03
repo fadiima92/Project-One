9e2b948290ce46184f95bb86981f12919ac4b332
RomanZ <rom.zaikin@gmail.com>
2019-10-03 11:59:18 -0700

    updates 10-3

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-<style>
-    h1 {
-        text-align: center;
-    }
+$("#search-btn").on("click", function() {
+    var nameYelp = $(this).attr("user-input");
 
+    // Perfoming an AJAX GET request to our queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
 
-</style>
+    // After the data from the AJAX request comes back
+      .then(function(response) {
 
+      // Saving the image_original_url property
+        var imageUrl = response.data.image_original_url;
+
+        // Creating and storing an image tag
+        var catImage = $("<img>");
+
+        // Setting the catImage src attribute to imageUrl
+        catImage.attr("src", imageUrl);
+        catImage.attr("alt", "cat image");
+
+        // Prepending the catImage to the images div
+        $("#images").prepend(catImage);
+      });
+  });
