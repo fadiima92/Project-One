cfc6482b24715475b062410128c91cd7482f7ade
Roman <rom.zaikin@gmail.com>
2019-10-04 10:28:27 -0700

    Yelp Api logic in console log

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+
 $("#searchBTN").on("click", function(event) {
     event.preventDefault();
-    var nameYelp = ("#userInput").text();
-    var queryURL = "https://api.yelp.com/v3/businesses/" + nameYelp + "/reviews?Bearer p2BCOzqktHtTdM68Yjo08bWKwPDdJacKL3kNyryQ43Eer5ICLMW8hXuqDt3uazvyJoyLDT_X-qzNvYV1w2CDeMRJQiE4d-o_fWTDfhtBgbnnAXExlmVfFYt7mfGUXXYx";
+    var nameYelp = $("#userInput").val();
+    console.log(nameYelp);
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972&term=" + nameYelp;
     $.ajax({
       url: queryURL,
+      headers: {
+        Authorization:'Bearer p2BCOzqktHtTdM68Yjo08bWKwPDdJacKL3kNyryQ43Eer5ICLMW8hXuqDt3uazvyJoyLDT_X-qzNvYV1w2CDeMRJQiE4d-o_fWTDfhtBgbnnAXExlmVfFYt7mfGUXXYx'
+    },
       method: "GET"
     })
       .then(function(response) {
