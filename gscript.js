cbd19961b4e85f9624649c9fe1f09b4923acd1b5
Michael Zaccaria <michael.zaccaria1@gmail.com>
2019-10-04 10:18:46 -0700

    seperating links

--- gscript.js
+++ gscript.js
@@ -2,7 +2,9 @@ function searchPlaces() {
 
     // setting variables for link to google
     var googlePlaces = "pizza"
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + googlePlaces + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBGe3kt3IzV8XbIQWDNC3AnkDTA1yWcdlQ";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" 
+    + googlePlaces + 
+    "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBGe3kt3IzV8XbIQWDNC3AnkDTA1yWcdlQ";
 
     // ajax call
     $.ajax({
