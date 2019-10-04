68356e85f3064561b230664d4e80fedb58d5f9a0
Michael Zaccaria <michael.zaccaria1@gmail.com>
2019-10-04 10:03:41 -0700

    starting search funtion for the google side

--- gscript.js
+++ gscript.js
@@ -1,6 +1,17 @@
-$.ajax({
-    url: "https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+function searchPlaces() {
+
+    // setting variables for link to google
+    var googlePlaces = $(this).attr("userInput");
+    var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + googlePlaces + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBGe3kt3IzV8XbIQWDNC3AnkDTA1yWcdlQ";
+
+    // ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+    });
+
+}
+
